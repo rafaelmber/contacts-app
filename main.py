f07d04703a1b4476719bca4e9762e8c3780dc61e
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2022-03-18 13:38:21 -0500

    Install mysql connector

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from database.db import mydb
 app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('public/index.html')
